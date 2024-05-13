a613eb530d8417922b12e8f95075942e5720d8d5
SzymonCieslar24 <108759086+SzymonCieslar24@users.noreply.github.com>
2024-05-13 19:22:45 +0200

    Update train.py

--- src/train.py
+++ src/train.py
@@ -7,5 +7,5 @@ model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 # Use the model
 model.train(data="data/animals.yaml", epochs=3)  # train the model
 metrics = model.val()  # evaluate model performance on the validation set
-results = model("test.png")  # predict on an image
+results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
 path = model.export(format="onnx")  # export the model to ONNX format
